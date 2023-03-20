f34a2310d82b5b22bd1a91b197398dcd6b872dff
HCricle <none>
2023-03-20 21:44:13 +0800

    new csv lib

---
+++ src/Ao.Middleware.Csv/CsvDataProvider.cs
@@ -0,0 +1,18 @@
+﻿using Ao.Middleware.DataWash;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ao.Middleware.Csv
+{
+    public class CsvDataProvider<TKey,TValue>:IDataProvider<TKey,TValue>
+    {
+        public CsvReader CsvReader { get; }
+
+        public void A()
+        {
+        }
+
+    }
+}
